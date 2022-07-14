58489eb35291c135765b9cb6002cf5b0f66301e8
Sharashvan Shrivastava <90495867+SHARASHVAN@users.noreply.github.com>
2022-07-15 01:18:28 +0530

    Delete hello.c

--- hello.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <cs50.h> //To declare the identifier
-int main(void)
-{
-    string name = get_string("What's your name? "); //To ask the name of user
-    printf("hello, %s\n", name); // To say hello to the user
-}
